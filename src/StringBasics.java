2f1e590563cea4f8191e3006eddf0c0ac79a0877
akashthanda14 <akashthanda14@gmail.com>
2025-09-28 22:58:52 +0530

    Method of String

--- src/StringBasics.java
+++ src/StringBasics.java
@@ -1,6 +1,6 @@
 public class StringBasics {
-    public static void main(String[] args) {
-        String s = "Akashdeep Thanda";
+    public static String revereseString(String s) {
+        // String s = "Akashdeep Thanda";
         char [] chars = s.toCharArray();
         int left = 0 , right = chars.length - 1;
         while(left < right){
@@ -8,6 +8,20 @@ public class StringBasics {
             chars[left++] = chars[right];
             chars[right--]=temp;
         }
-        System.out.println(new String(chars));
+        return new String(chars);
     }
+
+    public static void main(String[] args) {
+        String s = "Hello";
+        //Calling Two Pointer Reverse apporach
+        System.out.println(revereseString(s));
+        //Method to reverse String is reverseString(varibale);
+        System.out.println("The String Hello after Reversing is " + revereseString(s));
+        //Method to print the substring variable.subString(beiginIndex,endIndex);
+        System.out.println("The SubString is " + s.substring(0,4));
+        //Method to Concat the String with other string is variable.concat("String name");
+        System.out.println("The String after Concat is " + s.concat(" World"));
+    }
+
+
 }
